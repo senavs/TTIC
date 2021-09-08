dbdb42c5676b1d654785d38eb3b19ba0704ad7c0
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-09-08 20:25:36 -0300

    fix: fix typing hit

--- simulation/core/condition.py
+++ simulation/core/condition.py
@@ -2,16 +2,16 @@ from enum import Enum
 
 
 class Condition(Enum):
-    NORMAL: str = 'NORMAL'
-    EXPOSED: str = 'EXPOSED'
-    INFECTIOUS: str = 'INFECTIOUS'
-    HEALED: str = 'HEALED'
-    DEAD: str = 'DEAD'
+    NORMAL = 'NORMAL'
+    EXPOSED = 'EXPOSED'
+    INFECTIOUS = 'INFECTIOUS'
+    HEALED = 'HEALED'
+    DEAD = 'DEAD'
 
 
 class ConditionColor(Enum):
-    NORMAL: list = [255, 255, 255]
-    EXPOSED: list = [255, 69, 0]
-    INFECTIOUS: list = [139, 0, 0]
-    HEALED: list = [0, 255, 0]
-    DEAD: list = [0, 0, 0]
+    NORMAL = [255, 255, 255]
+    EXPOSED = [255, 69, 0]
+    INFECTIOUS = [139, 0, 0]
+    HEALED = [0, 255, 0]
+    DEAD = [0, 0, 0]
