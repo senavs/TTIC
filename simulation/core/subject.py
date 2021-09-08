d3e9bbf10e5835fd143a760849e6055473e805eb
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-09-08 20:26:33 -0300

    feat: start Subject and create SubjectSettings

---
+++ simulation/core/subject.py
@@ -0,0 +1,22 @@
+import random
+from itertools import count
+
+from simulation.settings import SubjectSettings
+from simulation.core.condition import Condition
+
+_instance_counter = count(1)
+
+
+class Subject:
+
+    def __init__(self):
+        self.id: int = next(_instance_counter)
+        self.age: int = random.randint(SubjectSettings.MIN_AGE, SubjectSettings.MAX_AGE + 1)
+        self.condition: Condition = Condition.NORMAL
+        self.healthy_lifestyle: float = random.random()
+
+    def agglomerate(self, subjects: list['Subject']):
+        pass
+
+    def __repr__(self):
+        return f'Subject({self.id}, {self.age}, {self.healthy_lifestyle:.2f})'
---
+++ simulation/settings.py
@@ -0,0 +1,3 @@
+class SubjectSettings:
+    MIN_AGE: int = 5
+    MAX_AGE: int = 90
