b993888e5521f7120558635f107410ed87eb9064
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-09-08 21:25:34 -0300

    feat: start Pathology and create PathologySettings

---
+++ simulation/core/pathology.py
@@ -0,0 +1,99 @@
+import random
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+from simulation.settings import SubjectSettings, PathologySettings
+from simulation.core.condition import Condition
+
+if TYPE_CHECKING:
+    from simulation.core.subject import Subject
+
+
+class Pathology(ABC):
+
+    def __init__(self, subject: 'Subject'):
+        self.infected_days: int = 0
+        self.subject = subject
+
+        self.prob_infection: float = PathologySettings.PROB_INFECTION
+        self.prob_death: float = PathologySettings.PROB_DEATH
+        self.exposed_max_day: float = PathologySettings.EXPOSED_MAX_DAY
+        self.infectious_max_day: float = PathologySettings.INFECTIOUS_MAX_DAY
+
+    @abstractmethod
+    def infect(self, subject: 'Subject') -> bool:
+        pass
+
+    @abstractmethod
+    def kill(self) -> bool:
+        pass
+
+    @abstractmethod
+    def evolve(self):
+        pass
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.infected_days})'
+
+
+class NullPathology(Pathology):
+
+    def infect(self, subject: 'Subject') -> bool:
+        return False
+
+    def kill(self) -> bool:
+        return False
+
+    def evolve(self):
+        pass
+
+
+class ConcretePathology(Pathology):
+
+    def increase_infected_days(self):
+        self.infected_days += 1
+
+    def infect(self, subject: 'Subject') -> bool:
+        if not self.should_infect(subject):
+            return False
+
+        subject.condition = Condition.EXPOSED
+        subject.pathology = ConcretePathology(subject)
+        return True
+
+    def should_infect(self, subject: 'Subject') -> bool:
+        # TODO: it has to consider prevention probs
+        if self.subject.condition not in (Condition.EXPOSED, Condition.INFECTIOUS):
+            return False
+        if subject.condition != Condition.NORMAL:
+            return False
+
+        return random.random() <= self.prob_infection
+
+    def kill(self) -> bool:
+        if not self.should_kill():
+            return False
+
+        self.subject.condition = Condition.DEAD
+        return True
+
+    def should_kill(self) -> bool:
+        # TODO: it has to consider prevention probs
+        if self.subject.condition not in (Condition.EXPOSED, Condition.INFECTIOUS):
+            return False
+
+        prob = (self.prob_death * (self.subject.age / SubjectSettings.MAX_AGE)) / self.subject.healthy_lifestyle
+        return random.random() <= prob
+
+    def evolve(self):
+        self.increase_infected_days()
+
+        if self.infected_days < self.exposed_max_day:
+            return
+
+        if self.subject.condition == Condition.EXPOSED and self.infected_days > self.exposed_max_day:
+            self.subject.condition = Condition.INFECTIOUS
+        elif self.subject.condition == Condition.INFECTIOUS and (self.infected_days - self.exposed_max_day) > self.infectious_max_day:
+            self.subject.condition = Condition.HEALED
+
+        self.kill()
--- simulation/settings.py
+++ simulation/settings.py
@@ -1,3 +1,10 @@
 class SubjectSettings:
     MIN_AGE: int = 5
     MAX_AGE: int = 90
+
+
+class PathologySettings:
+    PROB_INFECTION: float = 0.15
+    PROB_DEATH: float = 0.03
+    EXPOSED_MAX_DAY: int = 7
+    INFECTIOUS_MAX_DAY: int = 14
