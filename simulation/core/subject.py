57ee84822c6f3cf37207b30e9f2c4ee74ee374c3
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-09-11 13:45:58 -0300

    feat: create create_sick_subject and is_sick subject methods

--- simulation/core/subject.py
+++ simulation/core/subject.py
@@ -1,7 +1,7 @@
 import random
 from itertools import count
 
-from simulation.core.pathology import Pathology, NullPathology
+from simulation.core.pathology import ConcretePathology, Pathology, NullPathology
 from simulation.settings import SubjectSettings
 from simulation.core.condition import Condition
 
@@ -23,6 +23,16 @@ class Subject:
     def agglomerate(self, subjects: list['Subject']):
         for subject in subjects:
             self.pathology.infect(subject)
+
+    def is_sick(self) -> bool:
+        return self.condition in (Condition.EXPOSED, Condition.INFECTIOUS)
+
+    @classmethod
+    def create_sick_subject(cls, condition: Condition = Condition.EXPOSED) -> 'Subject':
+        subject = Subject()
+        subject.condition = condition
+        subject.pathology = ConcretePathology(subject)
+        return subject
 
     def __repr__(self):
         return f'Subject({self.id}, {self.age}, {self.healthy_lifestyle:.2f}, {self.pathology})'
