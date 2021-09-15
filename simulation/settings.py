4d98f882efc5008c23aa003a02009241ca0c2789
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-09-14 21:23:36 -0300

    feat: create printer and add grid border in each pixel

--- simulation/settings.py
+++ simulation/settings.py
@@ -1,3 +1,9 @@
+import os
+from uuid import uuid4
+
+simulation_uuid = uuid4()
+
+
 class SubjectSettings:
     MIN_AGE: int = 5
     MAX_AGE: int = 90
@@ -12,3 +18,14 @@ class PathologySettings:
 
 class BoardSettings:
     DIMENSION: int = 51
+
+
+class PrinterSettings:
+    OUTPUT_BOARD_RESOLUTION: tuple[int, int] = (800, 800)
+    OUTPUT_BOARD_DPI: int = 96
+    OUTPUT_BOARD_DIR: str = f'./results/{simulation_uuid}/board/'
+    OUTPUT_SHEET_DIR: str = f'./results/{simulation_uuid}/sheet/'
+
+
+os.makedirs(PrinterSettings.OUTPUT_BOARD_DIR, exist_ok=True)
+os.makedirs(PrinterSettings.OUTPUT_SHEET_DIR, exist_ok=True)
